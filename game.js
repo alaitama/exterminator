c547a585e59a16bb343f7b69df03ca26931572ae
Alberto Ramborg <ramborg@vbdebian.ramsys.es>
2013-02-04 21:11:12 +0100

    modified:   css/style.css 	modified:   game.js 	renamed:    index.html -> html5.html 	modified:   images/hero.png 	modified:   images/monster.png 	modified:   index.html 	modified:   persons.js 	begame.js 	begame/ 	images/bitmap.png 	images/bug.svg 	images/cement.jpg 	images/cement2.jpg 	images/cuca1.png 	images/cuca2.png 	images/cuca3.png 	images/cuca_64.png 	images/cuca_parking64.png 	images/cucasprite.png 	images/cucasprite_64.png 	images/hero3.png 	images/hero_old.png 	images/heroe_sprite42x47.png 	images/heroe_sprite64.png 	images/heroe_sprite80x90.png 	images/monster1.png 	images/monster_old.png 	zepto.js

--- game.js
+++ game.js
@@ -5,18 +5,23 @@
     var DEBUG = true;
 
     //VARIABLES GLOBALES
-    var sWidth = 512;
-    var sHeight = 480;
+    var WIDTH = 512;
+    var HEIGHT = 480;
 
     var iKeyUp = 38;
     var iKeyDown = 40;
     var iKeyLeft = 37;
     var iKeyRight = 39;
+    var iKeyPaused = 80; //p
+    
+    var PAUSED = false;
 
     //Mouse interact Hero
     var mouseX = 0;
     var mouseY = 0;
     var radians = 0;
+    var jumpX = 0;
+    var jumpY = 0;
 
     //Canvas element and Context
     var canvas;
@@ -25,7 +30,7 @@
     var fpsElement = null;
 
     //Time refresh
-    var then;
+    var lastAnimationFrameTime;
 
     // Background image
     var bgReady = false;
@@ -33,7 +38,7 @@
     bgImage.onload = function () {
         bgReady = true;
     };
-    bgImage.src = "./images/fondo.png";
+    bgImage.src = "./images/cement.jpg";
 
     //Hero image
     var heroReady = false;
@@ -41,7 +46,9 @@
     heroImage.onload = function () {
         heroReady = true;
     };
-    heroImage.src = "./images/hero.png";
+    //heroImage.src = "./images/hero.png";
+    //heroImage.src = "./images/heroe_sprite64.png";
+    heroImage.src = "./images/heroe_sprite42x47.png";
 
     //Monster image
     var monsterReady = false;
@@ -49,6 +56,7 @@
     monsterImage.onload = function () {
         monsterReady = true;
     };
+    //monsterImage.src = "./images/cucasprite_64.png";
     monsterImage.src = "./images/monster.png";
 
     //Bala image
@@ -60,18 +68,21 @@
     balaImage.src = "./images/bala.png";
         
     // Game objects
-    var hero = {
-        speed: 256, // movement in pixels per second
-        x: 0,
-        y: 0
-    };
+    var hero = null;
     
     //Variables globales
     var balaList = [];
     var monsterList = [];
-    var numMonsters = 5;
-    var timerShoot = 1000;
-    var idShootThread = null;
+    var numMonsters = 0;
+    
+    var timerShoot = 700;
+    //var idShootThread = null;
+    var lastAddShootTime = 0;
+    
+    
+    var timerMonster = 1000;
+    //var idMonsterThread = null;
+    var lastAddMonsterTime = 0;
     
     var monstersCaught = 0;
         
@@ -84,20 +95,36 @@
     };
     
     function loadSettings() {
-        numMonsters = document.forms[0].numMonsters.value;
+        timerMonster = document.forms[0].numMonsters.value;
         timerShoot = document.forms[0].timeShoot.value;
     };
 
     // Let's play this game!
-    initGame = function (animationMode) {
+    initGame = function () {
         
         // Create the canvas
         canvas = document.createElement("canvas");
         ctx = canvas.getContext("2d");
 
-        canvas.width = sWidth;
-        canvas.height = sHeight;
+        canvas.width = WIDTH;
+        canvas.height = HEIGHT;
         
+        
+        loadCanvas('gameDiv');
+        
+        initUI();
+        
+        fpsElement = document.getElementById('fps');
+        
+        reset();
+        lastAnimationFrameTime = Date.now();
+        
+        requestAnimationFrame(main);
+        
+    };
+    
+    initUI = function() {
+        /*
         addEventListener("keydown", function (e) {
             keysDown[e.keyCode] = true;
         }, false);
@@ -105,66 +132,194 @@
         addEventListener("keyup", function (e) {
             delete keysDown[e.keyCode];
         }, false);
+                
+        
+        canvas.addEventListener('dblclick', function(e) {
+            jumpX = e.clientX;
+            jumpY = e.clientY;
+            
+            if(DEBUG)
+                console.log("Jumped to: x=" + jumpX + ",y=" + jumpY);
+        }, true);
 
         canvas.addEventListener('mousemove', function(e) {
             mouseX = e.clientX;
-            mouseY = e.clientY;    
+            mouseY = e.clientY;
         }, true);
+        */
+        $('canvas').on('mousemove', onMouseMove);
+        $('canvas').on('dblclick', onDblClick);
+        $('canvas').on('mousedown', function(){ return false; }); //corrigue la seleccion de texto al doble click
         
-        loadCanvas('gameDiv');
+        $('canvas').on('focusout', onFocusOut);
         
-        fpsElement = document.getElementById('fps');
+        $(document).on('keydown', onKeyDown);
         
-        reset();
-        then = Date.now();
+        //Positions DIVs
+        var gameCanvas = $('canvas');
+        var splash = $('#splash');
+        var left = gameCanvas.css('left');
+        var width = gameCanvas.css('width');
+        var height = 100;
+        var top = gameCanvas.css('top');
         
-        requestAnimationFrame(main);
+        console.log(left + " " + top + " " + width + " " + height)
+        splash.css('left',0);
+        splash.css('top',0);
+        splash.css('width',width);
+        splash.css('height',height);
         
     };
     
+    onMouseMove = function(e) {
+        mouseX = e.clientX;
+        mouseY = e.clientY;
+    };
+    
+    onDblClick = function(e) {
+        e.preventDefault();
+        
+        jumpX = e.clientX;
+        jumpY = e.clientY;
+            
+        if(DEBUG)
+            console.log("Jumped to: x=" + jumpX + ",y=" + jumpY);
+    };
+    
+    onKeyDown = function(e) {
+        if(e.keyCode == iKeyPaused) {
+            PAUSED = !PAUSED;
+            if(PAUSED)
+                showPauseSplash();
+            else
+                hidePauseSplash();
+        }
+    }
+    
+    onFocusOut = function(e) {
+        PAUSED = true;
+        showPauseSplash();
+    }
+    
+    showPauseSplash = function() {
+        $('#splash').css('opacity','1.0');
+        $('#splash').css('display','block');
+    }
+    
+    hidePauseSplash = function() {
+        $('#splash').css('opacity','0.0');
+        $('#splash').css('display','none');
+    }
+    
+    
+    
     // Reset the game when the player catches a monster
     reset = function () {
-        loadSettings();
+        //loadSettings();
         
+        hero = new Hero(0,0);
         hero.x = canvas.width / 2;
         hero.y = canvas.height / 2;
 
-        for(var i=0;i<numMonsters;i++) {
-            
-            // Throw the monster somewhere on the screen randomly
-            var randx = 32 + (Math.random() * (canvas.width - 64));
-            var randy = 32 + (Math.random() * (canvas.height - 64));
-            var randIncX = Math.random() < 0.5 ? true : false;
-            var randIncY = Math.random() < 0.5 ? true : false;
-            
-            var newMonster = new  Monster(randx, randy, randIncX, randIncY);
-            monsterList.push(newMonster);
+        numMonsters = 0;
+        monstersCaught = 0;
+
+        /*
+        if(idMonsterThread!=null) {
+            window.clearInterval(idMonsterThread);
         }
+        idMonsterThread = setInterval(addMonster, timerMonster);
+        
         if(idShootThread!=null)
 			window.clearInterval(idShootThread);
         idShootThread = setInterval(shoot, timerShoot);
+        */
+        
     };
     
     // Update game objects
     var update = function (modifier) {
-        if (iKeyUp in keysDown) { // Player holding up
-            hero.y -= hero.speed * modifier;
+        
+        //Llamamos a procesos automaticos(equivale a setInterval manual)
+        updateThreads();
+        
+        //Si no esta saltando, giramos
+        if(jumpX==0 && jumpY==0) {
+            
+            hero.radiansDraw = Math.atan2(mouseX - hero.x, mouseY - hero.y - hero.height);
+            hero.radians = Math.atan2(mouseX - hero.x, mouseY - hero.y);
+            //var degree = (radians * (180 / Math.PI) * -1) + 180; 
         }
-        if (iKeyDown in keysDown) { // Player holding down
-            hero.y += hero.speed * modifier;
-        }
-        if (iKeyLeft in keysDown) { // Player holding left
-            hero.x -= hero.speed * modifier;
-        }
-        if (iKeyRight in keysDown) { // Player holding right
-            hero.x += hero.speed * modifier;
+        //Si esta saltando movemos
+        else {
+            if(!hero.jumping) {
+                hero.jumping = true;
+                hero.vx = jumpX - hero.x;
+                hero.vy = jumpY - hero.y - hero.height;
+
+                // Get absolute value of each vector
+                ax = Math.abs(hero.vx);
+                ay = Math.abs(hero.vy);
+
+                // Create a ratio
+                ratio = 1 / Math.max(ax, ay)
+                ratio = ratio * (1.29289 - (ax + ay) * ratio * 0.29289)
+
+                // Multiply by ratio
+                hero.vx = hero.vx * ratio * 300
+                hero.vy = hero.vy * ratio * 300
+                
+                ++hero.animFrame;
+                
+            }
+            
+            hero.x += hero.vx * modifier;
+            hero.y += hero.vy * modifier;
+            
+            hero.animTimer += modifier*1000;
+            if (hero.animTimer >= hero.animDelay) {
+                // Enough time has passed to update the animation frame
+                hero.animTimer = 0; // Reset the animation timer
+                ++hero.animFrame;
+                if (hero.animFrame >= hero.animNumFrames) {
+                    // We've reached the end of the animation frames; rewind
+                    hero.animFrame = 0;
+                    hero.jumping = false;
+                    jumpX = 0;
+                    jumpY = 0;
+                }
+            }
+            
         }
         
-        for(var i=0;i<numMonsters;i++)
-            monster_moving(monsterList[i], modifier);
-            
+        for(var i=0;i<numMonsters;i++) {
+            //monster_moving(monsterList[i], modifier);
+            monsterList[i].update(hero.x, hero.y, modifier);
+        }
+        
         shoot_moving(modifier);
 
+        //Motor de deteccion de colisiones
+        updateCollisions();
+        
+    };
+    
+    var updateThreads = function() {
+        var now = Date.now();
+        
+        if (now - lastAddMonsterTime > timerMonster) {
+		  lastAddMonsterTime = now;
+		  addMonster();
+        }
+        
+        if (now - lastAddShootTime > timerShoot) {
+		  lastAddShootTime = now;
+		  addShoot();
+        }
+    }
+    
+    var updateCollisions = function() {
+        
         //1.- Miramos si Balas tocan a Monsters
         var monsterListRemove = []; //Lista para elminar monsters tocados
         var balaListRemove = []; //Lista para eliminar balas que tocan monster
@@ -221,14 +376,14 @@
                 //reset();
             }
         }
-        
     };
+    
     
     //MONSTER_MOVIN
     var monster_moving = function (monster, modifier) {
         if (monster.incX) {
             monster.x += monster.speed * modifier;
-            if (monster.x + monster.width >= sWidth) {
+            if (monster.x + monster.width >= WIDTH) {
                 monster.incX = false;
             }
         }
@@ -241,7 +396,7 @@
 
         if (monster.incY) {
             monster.y += monster.speed * modifier;
-            if (monster.y + monster.height >= sHeight) {
+            if (monster.y + monster.height >= HEIGHT) {
                 monster.incY = false;
             }
         } else {
@@ -250,24 +405,53 @@
                 monster.incY = true;
             }
         }
+        
+        //Monster calc animSet
+        // Update hero animation
+        monster.animTimer += modifier * 1000;
+        if (monster.animTimer >= monster.animDelay) {
+            // Enough time has passed to update the animation frame
+            monster.animTimer = 0; // Reset the animation timer
+            ++monster.animFrame;
+            if (monster.animFrame >= monster.animNumFrames) {
+                // We've reached the end of the animation frames; rewind
+                monster.animFrame = 0;
+            }
+        }
     };
     
     //SHOOT
-    function shoot() {
+    function addShoot() {
         
-        var x = hero.x + heroImage.width/2;
-        var y = hero.y + heroImage.height/2;
-        var vx = mouseX - hero.x;
-        var vy = mouseY - hero.y;
-        var rad = radians;
-        
-        var newBala = new Shot(x,y,vx,vy,rad);
-        balaList.push(newBala);	
-        
-        if(DEBUG)
-            console.log("Num balas: " + balaList.length);
+        if(!hero.jumping) {
+            var x = hero.x;
+            var y = hero.y;
+            var vx = mouseX - hero.x;
+            var vy = mouseY - hero.y - hero.height;
+            var rad = hero.radians;
+            
+            var newBala = new Shot(x,y,vx,vy,rad);
+            balaList.push(newBala);	
+            
+            if(DEBUG)
+                console.log("Num balas: " + balaList.length);
+        }
         
     };
+    
+    //NEW MONSTER
+    function addMonster() {
+        // Throw the monster somewhere on the screen randomly
+        var randx = 32 + (Math.random() * (canvas.width - 64));
+        var randy = 32 + (Math.random() * (canvas.height - 64));
+        var randIncX = Math.random() < 0.5 ? true : false;
+        var randIncY = Math.random() < 0.5 ? true : false;
+            
+        var newMonster = new  Monster(randx, randy, randIncX, randIncY);
+        monsterList.push(newMonster);
+        numMonsters++;
+    }
+    
     
     //SHOOT_MOVING
     var shoot_moving = function (modifier) {
@@ -282,9 +466,9 @@
             
             //console.log(currentBala.x + ", " + currentBala.y);
             
-            if(currentBala.x < 0 || currentBala.x > sWidth)
+            if(currentBala.x < 0 || currentBala.x > WIDTH)
                 balaListRemove.push(i);
-            else if(currentBala.y < 0 || currentBala.y > sHeight)
+            else if(currentBala.y < 0 || currentBala.y > HEIGHT)
                 balaListRemove.push(i);
             
         }
@@ -303,24 +487,11 @@
     // Draw everything
     var render = function () {
         if (bgReady) {
-            ctx.drawImage(bgImage, 0, 0);
+            //ctx.drawImage(bgImage, 0, 0);
+            ctx.fillStyle = ctx.createPattern(bgImage, "repeat");
+            ctx.fillRect(0, 0, WIDTH, HEIGHT);
         }
 
-        if (heroReady) {
-            //ctx.drawImage(heroImage, hero.x, hero.y);
-            var centerHeroX = hero.x; // - heroImage.width/2;
-            var centerHeroY = hero.y; // - heroImage.height/2;
-            //console.log(heroImage.x);
-            radians = Math.atan2(mouseX - centerHeroX, mouseY - centerHeroY);
-            var degree = (radians * (180 / Math.PI) * -1) + 90; 
-            //ctx.rotate(Math.PI / 180 * 0.5); // 1/2 a degree
-            //console.log(degree);
-            ctx.save();
-            ctx.translate(centerHeroX, centerHeroY); 
-            ctx.rotate(-radians); 
-            ctx.drawImage(heroImage,0 - heroImage.width/2 ,0 - heroImage.height/2);
-            ctx.restore();
-        }
         
         if (monsterReady) {
             for(var i=0;i<numMonsters;i++) {
@@ -334,6 +505,12 @@
             for(var i=0;i<numBalas;i++) {
                 balaList[i].draw(ctx, balaImage);
             }
+        }
+        
+        if (heroReady) {
+            
+            hero.draw(ctx, heroImage);
+
         }
 
         // Score
@@ -351,27 +528,30 @@
     function loadFps(now) {
 	   if (now - lastFpsUpdateTime > 1000) {
 		  lastFpsUpdateTime = now;
-		  fpsElement.innerHTML = calculateFps(now, then) + ' fps';
+		  fpsElement.innerHTML = calculateFps(now, lastAnimationFrameTime) + ' fps';
 	   }
 
-	   return fps;
+	   //return fps;
 	}
     
     // The main game loop
     var main = function () {
         var now = Date.now();
-        var delta = now - then;
+        var delta = now - lastAnimationFrameTime;
         
-		loadFps(now);
-        update(delta / 1000);
-        render();
+        if(fpsElement!=null && fpsElement!=undefined)
+            loadFps(now);
+        
+        if(!PAUSED) {
+            update(delta / 1000);
+            render();
+        }
 
-        then = now;
+        lastAnimationFrameTime = now;
         requestAnimationFrame(main);
     };
 
-        
-    
+
 }(window));
 //FIN 
 
--- persons.js
+++ persons.js
@@ -1,19 +1,171 @@
 /*
+ * HERO CLASS
+ */
+var Hero = function(posX, posY) {
+    var self = this;
+    self.x = posX,
+	self.y = posY,
+	self.width = 42,// 64,
+	self.height = 47, // 64,
+	self.radians = 0; 
+    self.radiansDraw = 0;
+	// Animation settings
+    self.jumping = false,
+    self.vx = 0,
+    self.vy = 0,
+	self.animSet = 4,
+	self.animFrame = 0,
+	self.animNumFrames = 4,
+	self.animDelay = 200,
+	self.animTimer = 0
+    
+}
+
+Hero.prototype.draw = function(ctx, heroImage) {
+    var self = this;
+    
+    //console.log("DIBUJA " + self.x + ","+self.y)
+    
+    var spriteX = (this.animFrame * this.width);
+    
+    var centerHeroX = self.x;
+    var centerHeroY = self.y;
+    
+    // Render image to canvas
+    ctx.save();
+    ctx.translate(centerHeroX, centerHeroY); 
+    ctx.rotate(-self.radiansDraw); 
+    
+    
+    ctx.drawImage(
+        heroImage,
+        spriteX, 0, self.width, self.height,
+        0 - self.width/2, 0 - self.height/2, self.width, self.height
+    );
+    
+    //ctx.drawImage(heroImage, 0 - self.width/2, 0 - self.height/2);
+    
+    ctx.restore();
+    
+    //ctx.drawImage(heroImage, self.x, self.y);
+        
+}
+
+
+/*
+ * MONSTER CLASS
+ */
+var Monster1 = function (posX, posY, incX, incY){
+    var self = this;
+    self.x = posX,
+    self.y = posY,
+    self.width = 62,
+    self.height = 64,
+    self.speed = 128,
+    self.incX = incX,
+    self.incY = incY;
+    self.animFrame = 0;
+    self.animNumFrames = 4;
+    self.animDelay = 200;
+    self.animTimer = 0;
+}
+
+Monster1.prototype.draw = function(ctx, monsterImage) {
+        //ctx.drawImage(monsterImage, this.x, this.y);
+        
+        // Determine which part of the sprite sheet to draw from
+        /*
+		var spriteX = (
+			(this.animSet * (this.width * this.animNumFrames)) +
+			(this.animFrame * this.width)
+		);
+        */
+        
+        var spriteX = (this.animFrame * this.width);
+        
+        // Render image to canvas
+		ctx.drawImage(
+			monsterImage,
+			spriteX, 0, this.width, this.height,
+			this.x, this.y, this.width, this.height
+		);
+}
+
+/*
  * MONSTER CLASS
  */
 var Monster = function (posX, posY, incX, incY){
     var self = this;
+    self.width = 42,
+    self.height = 50,
+    
     self.x = posX,
     self.y = posY,
-    self.width = 25,
-    self.height = 25,
+    self.radians = 0,
     self.speed = 128,
+    self.noChangeDirTimer = 0,
+    self.noChangeDirDelay = 400,
+    self.stopTimer = 0,
+    self.stopDelay = 2000,
+    self.vx = 0,
+    self.vy = 0,
+    
     self.incX = incX,
     self.incY = incY;
+    
+    self.animFrame = 0;
+    self.animNumFrames = 4;
+    self.animDelay = 200;
+    self.animTimer = 0;
 }
 
 Monster.prototype.draw = function(ctx, monsterImage) {
-        ctx.drawImage(monsterImage, this.x, this.y);
+    var self = this;
+    //ctx.drawImage(monsterImage, this.x, this.y);
+        
+    // Determine which part of the sprite sheet to draw from
+    /*
+	var spriteX = (
+		(this.animSet * (this.width * this.animNumFrames)) +
+		(this.animFrame * this.width)
+	);
+    */
+        
+    var spriteX = (0 * this.width);
+        
+    // Render image to canvas
+    ctx.save();
+    ctx.translate(self.x, self.y); 
+    ctx.rotate(-self.radians); 
+        
+    ctx.drawImage(
+        monsterImage,
+        spriteX, 0, self.width, self.height,
+        0 - self.width/2, 0 - self.height/2, self.width, self.height
+    );
+    ctx.restore();
+        
+	
+}
+
+Monster.prototype.update = function(heroX, heroY, modifier) {
+    this.noChangeDirTimer += modifier*1000;
+    if (this.noChangeDirTimer >= this.noChangeDirDelay) {
+        // Enough time has passed to update the animation frame
+        this.stopTimer += modifier*1000;
+        this.vx = 0, this.vy = 0;
+        if(this.stopTimer >= this.stopDelay) {
+            this.noChangeDirTimer = 0; // Reset the animation timer
+            this.stopTimer = 0;
+            this.vx = heroX - this.x;
+            this.vy = heroY - this.y;
+            this.radians = Math.atan2(heroX - this.x, heroY - this.y);
+        }
+    }
+    
+    this.x = this.x + this.vx*modifier;
+    this.y = this.y + this.vy*modifier;
+    
 }
 
 
@@ -36,6 +188,7 @@ Shot.prototype.draw = function(ctx, balaImage) {
     ctx.translate(this.x, this.y); 
     ctx.rotate(-this.rad); 
     ctx.drawImage(balaImage,0 - balaImage.width/2 ,0 - balaImage.height/2);
+    //ctx.drawImage(balaImage,0 ,0);
     ctx.restore();
 }
 
